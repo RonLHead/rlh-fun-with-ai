b6842b4c8c217d381360c6ef827c1ce25d1c2757
Ronald Head <ronaldlhead@gmail.com>
2022-05-16 15:22:20 -0600

    Refactor Responses elements

--- src/components/Responses/Responses.js
+++ src/components/Responses/Responses.js
@@ -4,16 +4,16 @@ import './Responses.css';
 function Responses() {
     return(
         <div>
-          <header>
+          <header className='Responses-heading'>
               <h2>Responses</h2>
           </header>
           <section className='Responses-section'>
               <div className='sub-heading'>
-                <p>Prompt:</p>
+                <p className='sub-heading-margin'>Prompt:</p>
                 <p>Response:</p>
               </div>
               <div className='Responses-container'>
-                <p>Prompt goes here</p>
+                <p className='Responses-margin'>Prompt goes here</p>
                 <p>Response goes here</p>
               </div>
           </section>
