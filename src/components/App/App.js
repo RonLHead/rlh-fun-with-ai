deec8733526523dcce960390eb88d8c0410e7e0e
Ronald Head <ronaldlhead@gmail.com>
2022-05-16 13:39:01 -0600

    Set up App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import Form from '../Form/Form';
+import Responses from '../Responses/Responses';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main className='App'>
+      <header >  
+          <h1 >Fun with AI</h1>
       </header>
-    </div>
+      <Form />
+      <Responses />
+    </main>
   );
 }
 
 export default App;
+
