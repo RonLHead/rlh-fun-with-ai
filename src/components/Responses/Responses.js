7cbcafa7b228ce6a7c1fe0f3c4ebbe90b525cbfe
Ronald Head <ronaldlhead@gmail.com>
2022-05-16 15:11:00 -0600

    Refactor display of Responses component

--- src/components/Responses/Responses.js
+++ src/components/Responses/Responses.js
@@ -4,12 +4,19 @@ import './Responses.css';
 function Responses() {
     return(
         <div>
-        <header>
-            <h2 className='Responses-heading'>Responses</h2>
-        </header>
-        <section className='Responses-section'>
-            <p>Responses go here</p>
-        </section>
+          <header>
+              <h2>Responses</h2>
+          </header>
+          <section className='Responses-section'>
+              <div className='sub-heading'>
+                <p>Prompt:</p>
+                <p>Response:</p>
+              </div>
+              <div className='Responses-container'>
+                <p>Prompt goes here</p>
+                <p>Response goes here</p>
+              </div>
+          </section>
         </div>
     )
 }
