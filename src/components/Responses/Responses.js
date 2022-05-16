307cee5ec37dcc02c9db0b1d6fc585544a724252
Ronald Head <ronaldlhead@gmail.com>
2022-05-16 14:51:26 -0600

    Set up Responses directory

---
+++ src/components/Responses/Responses.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Responses.css';
+
+function Responses() {
+    return(
+        <div>
+        <header>
+            <h2 className='Responses-heading'>Responses</h2>
+        </header>
+        <section className='Responses-section'>
+            <p>Responses go here</p>
+        </section>
+        </div>
+    )
+}
+
+export default Responses;
